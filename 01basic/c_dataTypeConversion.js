57a473bc3d66ab462878f4720e8897341f4ffec9
codesourcepro <85899702+codesourcepro@users.noreply.github.com>
2023-12-16 12:00:28 +0000

    data Type conversion

--- 01basic/c_dataTypeConversion.js
+++ 01basic/c_dataTypeConversion.js
@@ -17,3 +17,25 @@ console.log(typeof(defNull));
 let changeInNumber = Number(defNull)
 console.log(typeof changeInNumber);
 console.log(changeInNumber);
+
+
+let defUndefined = undefined;
+console.log(typeof(defUndefined));
+
+let undefinedInNumber = Number(defUndefined)
+console.log(typeof undefinedInNumber);
+console.log(undefinedInNumber);
+
+
+
+// advanced 1-> true, 0 -> false, " " -> false, "csp" -> true
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+let rand = 55
+let strinNumber = String(rand)
+console.log(strinNumber);
+console.log(typeof strinNumber);
