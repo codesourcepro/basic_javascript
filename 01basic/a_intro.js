4ffda5548771774b707948864046f28b6cfc4c50
codesourcepro <85899702+codesourcepro@users.noreply.github.com>
2023-12-16 11:44:30 +0000

    basic commit

---
+++ 01basic/a_intro.js
@@ -0,0 +1,6 @@
+/*
+1.What is Javascript?
+2. Data Type
+
+
+*/
---
+++ 01basic/c_dataTypeConversion.js
@@ -0,0 +1,19 @@
+/*
+DataType Conversion in JavaScript
+
+*/
+
+let score = "18ABC";
+console.log(typeof(score));
+
+let valueInNumbebr = Number(score)
+console.log(typeof valueInNumbebr);
+console.log(valueInNumbebr);
+
+
+let defNull = null;
+console.log(typeof(defNull));
+
+let changeInNumber = Number(defNull)
+console.log(typeof changeInNumber);
+console.log(changeInNumber);
