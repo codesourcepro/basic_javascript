910f7722ee730625785e9a3a506f7789ee342b01
codesourcepro <85899702+codesourcepro@users.noreply.github.com>
2023-12-16 18:55:01 +0000

    avoid this type of comparision

---
+++ 01basic/d_comparision.js
@@ -0,0 +1,19 @@
+/*
+Comparision of Datatypes
+*/
+
+console.log("2" > 1);
+console.log("007" > 1);
+
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+
+console.log(undefined == 0);
+console.log(undefined < 0);
+console.log(undefined > 0);
+
+
+console.log("2" === 2);
