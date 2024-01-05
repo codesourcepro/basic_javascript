74d47e65f7f92c435c00877a08e7468edbd4f284
codesourcepro <85899702+codesourcepro@users.noreply.github.com>
2024-01-05 19:56:58 +0000

    Funtion basic, Syntax, and rules with examples

---
+++ 04Functions/a_function.js
@@ -0,0 +1,42 @@
+// Functions are subprograms which are used to compute a value or perfrom a task.
+
+/*
+Syntax:
+
+    function function_name(){
+        block of statement
+    }
+
+example:
+    function channel(){
+        console.log("CodeSourcePro")
+    }
+
+
+Rules:
+    - Function name only starts with a letter, an underscore(_).
+        function bank(){
+            console.log("World Bank")
+        }
+
+        function _name(){
+            console.log("CodeSourcePro")
+        }
+
+    - Function name cannot start with number.
+        function 123Street(){
+            console.log("Its generate error.")
+        }
+    - Do not use reserved keyword.
+        function this(){
+            console.log("Reserved keyword not allowed")
+        }
+    - Function names are case-sensitive.
+        function Differnt(){
+            console.log("Capital Letter")
+        }
+        funtion different(){
+            console.log("Small Letter")
+        }
+
+*/
