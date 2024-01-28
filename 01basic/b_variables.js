aa9f0bf7be5eddb0e09d60aaa950e64b4a4245c5
codesourcepro <85899702+codesourcepro@users.noreply.github.com>
2024-01-28 19:44:43 +0000

    vaiables

--- 01basic/b_variables.js
+++ 01basic/b_variables.js
@@ -1,3 +1,17 @@
 /*
+varialbe
+JavaScript variables are powerful programming constructs. However, there is a convention that should be 
+followed by creating a variable in JavaScript.
+
+Following are the rules that a Developer should follow to properly get variables declared:
+- Variable name should only contain alphabets, numbers, $ and _
+- Variable name should not start with a number
+- Variable names are case-sensitive i.e. result and Result are two different variables
+- Variables can’t be named as one of the reserved keywords like let, return, const, etc.
+- Variable should use camelCase i.e. numOne is preferred over NumOne or numone
+- Variable can’t have hyphen - in its name
+- Use easy-to-understand names that symbolize the value stored in variables. 
+    For e.g. instead of calling a variable phNum, a Developer can call it phoneNumber.
+- Don’t use single-letter variable names like x, a, z, etc.
 
 */
